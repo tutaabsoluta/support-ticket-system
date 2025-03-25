bef36bc50504849a9f854850bb87ceeaa67c1848
Sergio Duran <538ser@gmail.com>
2025-03-25 08:27:26 -0600

    Cors options

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const envs = {
-    // FRONTEND_URL: get('FRONTEND_URL').required().asUrlString(),
+    FRONTEND_URL: get('FRONTEND_URL').required().asUrlString(),
     PORT: get('PORT').required().asPortNumber(),
 };
